2ca22423dceb281206a9bbf4a7815b613558f0fa
gasdsba <sbaqueroa@gmail.com>
2017-12-22 11:36:21 -0500

    Heroku and local config properties.

--- src/main/java/co/com/sbaqueroadev/contigo/config/MongoConfig.java
+++ src/main/java/co/com/sbaqueroadev/contigo/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package co.com.sbaqueroadev.contigo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -13,10 +14,16 @@ import com.mongodb.MongoClientURI;
 @EnableMongoRepositories(basePackages = "co.com.sbaqueroadev.contigo.dao")
 public class MongoConfig extends AbstractMongoConfiguration {
   
+		@Autowired
+		OwnProperties properties;
+	
     @Override
     protected String getDatabaseName() {
-        //return "contigo";
-    	return "heroku_0gfrmdn7";
+    	if(properties.isHeroku()){        
+    		return "heroku_0gfrmdn7";
+    	}else{
+    		return "contigo";
+    	}
     }
   
     @Override
@@ -29,7 +36,11 @@ public class MongoConfig extends AbstractMongoConfiguration {
 			//mongodb://heroku_0gfrmdn7:or0uhb6nr33ig0e1o8l1klvd8l@ds163826.mlab.com:63826/heroku_0gfrmdn7
 			//return new MongoClient("127.0.0.1", 27017);
 			//return new MongoClient("mongodb://heroku_0gfrmdn7:or0uhb6nr33ig0e1o8l1klvd8l@ds163826.mlab.com:63826/heroku_0gfrmdn7");
-			return new MongoClient(new MongoClientURI(System.getenv("MONGODB_URI")));
+			if(properties.isHeroku()){
+				return new MongoClient(new MongoClientURI(System.getenv("MONGODB_URI")));
+			}else{
+				return new MongoClient("127.0.0.1", 27017);
+			}
 		}
 		
 		@Bean
